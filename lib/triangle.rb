e5bf9b2e0dd71afe36a55c7945bfaad11536e815
Emmanuel <rivera128@live.com>
2020-11-22 01:22:49 -0600

    commit tree 7f3f17ae7a9bd85458d879f12ae07b384feca08d

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,31 @@
 class Triangle
-  # write code here
+  attr_accessor :length_one, :length_two, :length_three
+
+  def initialize(length_one, length_two, length_three)
+    @length_one = length_one
+    @length_two = length_two
+    @length_three = length_three
+    @sides = [length_one, length_two, length_three].sort
+  end
+
+  def kind
+    if (length_one <= 0 || length_two <= 0 || length_three <0) || @sides[0] + @sides[1] <= @sides[2]
+      raise TriangleError
+    elsif length_one == length_two && length_two == length_three
+      :equilateral
+    elsif length_one == length_two || length_two == length_three || length_three == length_one
+      :isosceles
+    elsif length_one != length_two && length_two != length_three && length_three != length_two
+      :scalene
+    elsif length_one == length_two && length_two == length_three
+      :equilateral
+    end
+  end
+
+
+  class TriangleError < StandardError
+    def error_message
+      puts "Not a triangle!!!!!!!!!!!!"
+    end
+  end
 end
